59b48e81daac80c9ffb4d64d668ea87ae167ce38
LAPTOP-TOHPCICG\Thảo Quảng <quang.tranhaithao@hcmut.edu.vn>
2024-09-30 09:50:26 +0700

    Ex1 version 2

--- Coding/Core/Src/exercise1.c
+++ Coding/Core/Src/exercise1.c
@@ -8,16 +8,16 @@
 #include "exercise1.h"
 
 void init_exercise1(){
-	setTimer(0, 50);
+	setTimer(0, 0);
 	resetAllDigits();
 }
 
 int num = 1;
 int idx = 0;
 void exercise1_run(){
-	setDigit(idx);
-	display7SEG(num);
 	if (timer_flag[0] == 1){
+		setDigit(idx);
+		display7SEG(num);
 		setTimer(0, 50);
 		switch (idx) {
 			case 0: {
